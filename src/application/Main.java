ae71f09ee90e77a02eb02643c8da15b9e148cfef
Miguel nascimento <miguel_tobi@hotmail.com>
2021-05-20 23:11:21 -0300

    apertfeiçoamento da primeira tela e chamada do about

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,13 +21,17 @@ public class Main extends Application {
 			ScrollPane.setFitToHeight(true);
 			ScrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(ScrollPane);
+			mainScene = new Scene(ScrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Teste pulse");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene; 
 	}
 
 	public static void main(String[] args) {
